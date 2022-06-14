de3ede11bb0154a10e068a735fec7e57e9cd9db5
Aishahassan1 <aishahassan_5@hotmail.com>
2022-06-15 00:09:08 +0100

    cleaned code

--- index.js
+++ index.js
@@ -36,6 +36,3 @@ btnEl.addEventListener('click', function convert() {
 })
 
 
-// 20 meters = 65.616 feet | 20 feet = 6.096 meters
-//20 liters = 5.284 gallons | 20 gallons = 75.708 liters
-// 20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
