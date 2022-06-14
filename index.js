bf5828f3ed35be4d8a705e9f6a695d2777d90c75
Aishahassan1 <aishahassan_5@hotmail.com>
2022-06-15 00:03:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// /*
+// 1 meter = 3.281 feet
+// 1 liter = 0.264 gallon
+// 1 kilogram = 2.204 pound
+// */
+
+const inputEl = document.querySelector(".input-el")
+
+const btnEl = document.querySelector(".btn-el")
+
+let group1 = document.querySelector(".group-1 p") 
+let group2 = document.querySelector(".group-2 p")
+let group3 = document.querySelector(".group-3 p")
+
+btnEl.addEventListener('click', function convert() {
+    
+    let num = inputEl.value;
+    
+    const metres = inputEl.value * 3.281
+    const feet = inputEl.value / 3.281
+
+    const litres = inputEl.value * 0.264
+    const gallon = inputEl.value / 0.264
+
+    const kilograms = inputEl.value * 2.204
+    const pounds = inputEl.value / 2.204
+
+
+   group1.innerHTML = `<p> ${num} metres = ${metres.toFixed(3)} | ${num} = ${feet.toFixed(3)} </p>`  
+      
+   group2.innerHTML = `<p> ${num} litres = ${litres.toFixed(3)} | ${num} = ${gallon.toFixed(3)} </p>`  
+   
+   group3.innerHTML = `<p> ${num} kilograms = ${kilograms.toFixed(3)} | 
+   ${num} = ${pounds.toFixed(3)} </p>` 
+   
+})
+
+
+// 20 meters = 65.616 feet | 20 feet = 6.096 meters
+//20 liters = 5.284 gallons | 20 gallons = 75.708 liters
+// 20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
